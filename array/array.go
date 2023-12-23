61e3532b4c4d6238ba3f96f014e52052d9331719
Yash Kale <imyashkale@gmail.com>
2023-12-23 12:38:29 +0530

    Array: LinearSearch, Lowest, Largest Implementation

--- array/array.go
+++ array/array.go
@@ -1,12 +1,58 @@
 package array
 
+import "math"
 
 type Array struct {
-	Elements []int
-	Len int
+	elements []int
+	len      int
 }
 
-func NewArray() *Array {
+// NewArray Create the array
+func New(size int) *Array {
 	return &Array{
+		elements: make([]int, size),
+		len:      size,
 	}
+}
+
+func (a *Array) With(arr []int) *Array {
+	a.elements = arr
+	return a
+}
+
+// LinearSearch
+func (a Array) LinearSearch(target int) (int){
+	for i := 0; i < a.Len(); i++ {
+		if target == a.elements[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+// Len Returns the len of the array
+func (a Array) Len() int {
+	return a.len
+}
+
+// Largest Find the largest Number in array/ Maximum No
+func (a Array) Largest() int {
+	largest := int(math.Inf(-1))
+	for i := 0; i < a.Len(); i ++ {
+		if largest < a.elements[i] {
+			largest = a.elements[i]
+		} 
+	}
+	return largest
+}
+
+// Lowest Find the lowest Element in the array
+func (a Array) Lowest() int {
+	lowest := int(math.Inf(1))
+	for i := 0; i < a.Len(); i ++ {
+		if lowest > a.elements[i] {
+			lowest = a.elements[i]
+		}
+	}
+	return lowest
 }
--- array/array_test.go
+++ array/array_test.go
@@ -8,6 +8,40 @@ import (
 )
 
 func TestArray(t *testing.T) {
-	a := array.NewArray()
-	assert.Equal(t, a.Len, 0)
+	a := array.New(0)
+	assert.Equal(t, a.Len(), 0)
+}
+
+func TestLinearSearchFound(t *testing.T){
+	a := array.New(10).With([]int{1,2,3,4,5,6,7,8,9,10})
+	
+	targetIndex := a.LinearSearch(2)
+	assert.Equal(t, targetIndex, 1)
+	assert.Equal(t, a.Len(), 10)
+}
+
+func TestLinearSearchNotFound(t *testing.T){
+	a := array.New(4).With([]int{1,2,3,4})
+	
+	targetIndex := a.LinearSearch(6)
+	assert.Equal(t, targetIndex, -1)
+	assert.Equal(t, a.Len(), 4)
+}
+
+func TestLargest(t *testing.T){
+
+	a := array.New(6).With([]int{1,2,3,4,5,6})
+
+	largest := a.Largest()
+	assert.Equal(t, largest, 6)
+	assert.Equal(t, a.Len(), 6)
+}
+
+func TestLowest(t *testing.T) {
+
+	a := array.New(4).With([]int{3,4,5,1})
+
+	lowest := a.Lowest()
+	assert.Equal(t, lowest, 1)
+	assert.Equal(t, a.Len(), 4)
 }
